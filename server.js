52d191a5a05b9833f9c0e9412f18a888a620c2a7
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2016-03-19 00:37:58 +1100

    Better connection management and socket id logging

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
   
   socket.on('new widget', function(widget){
     io.emit('broadcast widget', widget);
--- src/app/services/widget-socket.ts
+++ src/app/services/widget-socket.ts
@@ -3,17 +3,25 @@ import * as io from 'socket.io-client';
 
 @Injectable()
 export class WidgetSocket {
-  private socket: SocketIOClient.Socket;
+    private socket: SocketIOClient.Socket;
 
-  connect() {
-    this.socket = io.connect('localhost:3001')
-  }
-  
-  disconnect() {
-    this.socket.close();
-  }
-  
-  addWidget(widget) {
-    
-  }
+    connect() {
+        this.socket = io.connect('localhost:3001')
+        this.socket.on('connect', () => {
+            console.log('Opened a new socket connection', this.socket.id);
+        })
+    }
+
+    disconnect() {
+        console.log('Closing socket connection', this.socket.id);
+        this.socket.close();
+    }
+
+    addWidget(widget) {
+        this.socket.emit('add widget', widget);
+    }
+
+    registerAddWidget() {
+
+    }
 }
